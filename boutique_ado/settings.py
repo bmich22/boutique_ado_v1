0b239733a77bda29b7ca5db9a5e17b1d69ed4ca4
bmich22 <barbmichel22@gmail.com>
2025-05-04 22:12:10 +0200

    Create database and deploy to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
-
 if os.path.isfile('env.py'):
-    import env
+    import env  # flake8 will throw an error here, but it is necessary to import env.py
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x0oxg0ew-(63o49zfc2k%$85yuvq_gruz63yw^s#sq5nrg8)yz'
+# SECRET_KEY = 'django-insecure-x0oxg0ew-(63o49zfc2k%$85yuvq_gruz63yw^s#sq5nrg8)yz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',  # vscode preview
     'localhost',  # listen for stripe webhooks
+    'bmich-boutique-ado-546aa0a0b5e9.herokuapp.com'
 ]
 
 
@@ -122,16 +124,20 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
